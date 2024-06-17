614f2e921c7b8c9e0b74b4168b5b4d0126aa9f12
springhana <xksxksanfro7@naver.com>
2024-06-17 22:47:51 +0900

    ⚙️setting:  버밸 경로 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    ['@babel/plugin-transform-class-properties', {loose: false}],
+    ['@babel/plugin-transform-private-methods', {loose: false}],
+    ['@babel/plugin-transform-private-property-in-object', {loose: false}],
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
