bd4b80ce028501c05fa379b60e0fe10ab2591721
springhana <xksxksanfro7@naver.com>
2024-06-27 18:57:15 +0900

    ⚙️setting:  바벨  설정

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,9 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
-    ['@babel/plugin-transform-class-properties', {loose: false}],
-    ['@babel/plugin-transform-private-methods', {loose: false}],
-    ['@babel/plugin-transform-private-property-in-object', {loose: false}],
+    ['@babel/plugin-proposal-private-methods', {loose: true}],
+    ['@babel/plugin-proposal-class-properties', {loose: true}],
+    ['@babel/plugin-proposal-private-property-in-object', {loose: true}],
     [
       'module-resolver',
       {
