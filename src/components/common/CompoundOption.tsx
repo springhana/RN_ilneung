b1c240676d2dab5ca62938152d86c40cd9236e57
springhana <xksxksanfro7@naver.com>
2024-06-30 19:07:28 +0900

    ✨feat:  합성 컴포넌트 구현

---
+++ src/components/common/CompoundOption.tsx
@@ -0,0 +1,271 @@
+import {PropsWithChildren, ReactNode, createContext, useContext} from 'react';
+import {
+  Modal,
+  ModalProps,
+  Pressable,
+  PressableProps,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  GestureResponderEvent,
+} from 'react-native';
+
+import {colors} from '@/constants/colors';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import useThemeStore from '@/store/useThemeStore';
+import {ThemeMode} from '@/types';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+interface OptionModalProps extends ModalProps {
+  children: ReactNode;
+  isVisible: boolean;
+  hideOption: () => void;
+  animateType?: ModalProps['animationType'];
+}
+
+interface OptionContextValue {
+  onClickOutSide?: (event: GestureResponderEvent) => void;
+}
+
+const OptionContext = createContext<OptionContextValue | undefined>(undefined);
+
+function OptionModal({
+  children,
+  isVisible,
+  hideOption,
+  animateType = 'slide',
+  ...props
+}: OptionModalProps) {
+  const onClickOutSide = (event: GestureResponderEvent) => {
+    if (event.target === event.currentTarget) {
+      hideOption();
+    }
+  };
+
+  return (
+    <Modal
+      visible={isVisible}
+      transparent={true}
+      animationType={animateType}
+      onRequestClose={hideOption}
+      {...props}>
+      <OptionContext.Provider value={{onClickOutSide}}>
+        {children}
+      </OptionContext.Provider>
+    </Modal>
+  );
+}
+
+function Background({children}: PropsWithChildren) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+  const optionContext = useContext(OptionContext);
+
+  return (
+    <SafeAreaView
+      style={styles.optionBackground}
+      onTouchEnd={optionContext?.onClickOutSide}>
+      {children}
+    </SafeAreaView>
+  );
+}
+
+function Container({children}: PropsWithChildren) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
+  return <View style={styles.optionContainer}>{children}</View>;
+}
+
+interface ButtonProps extends PressableProps {
+  children: ReactNode;
+  isDanger?: boolean;
+  isChecked?: boolean;
+}
+
+function Button({
+  children,
+  isDanger = false,
+  isChecked = false,
+  ...props
+}: ButtonProps) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
+  return (
+    <Pressable
+      style={({pressed}) => [
+        pressed && styles.optionButtonPressed,
+        styles.optionButton,
+      ]}
+      {...props}>
+      <Text style={[styles.optionText, isDanger && styles.dangerText]}>
+        {children}
+      </Text>
+
+      {isChecked && (
+        <Ionicons name="checkmark" size={20} color={colors[theme].BLUE_500} />
+      )}
+    </Pressable>
+  );
+}
+
+function Title({children}: PropsWithChildren) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
+  return (
+    <View style={styles.titleContainer}>
+      <Text style={styles.titleText}>{children}</Text>
+    </View>
+  );
+}
+
+function Divider() {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
+  return <View style={styles.border}></View>;
+}
+
+interface CheckBoxProps extends PressableProps {
+  children: ReactNode;
+  icon?: ReactNode;
+  isChecked?: boolean;
+}
+
+function CheckBox({
+  children,
+  icon,
+  isChecked = false,
+  ...props
+}: CheckBoxProps) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
+  return (
+    <Pressable
+      style={({pressed}) => [
+        pressed && styles.optionButtonPressed,
+        styles.checkBoxContainer,
+      ]}
+      {...props}>
+      <Ionicons
+        name={`checkmark-circle${isChecked ? '' : '-outline'}`}
+        size={22}
+        color={colors[theme].BLUE_500}
+      />
+      {icon}
+      <Text style={styles.checkBoxText}>{children}</Text>
+    </Pressable>
+  );
+}
+
+interface FilterProps extends PressableProps {
+  children: ReactNode;
+  isSelected?: boolean;
+}
+
+function Filter({children, isSelected, ...props}: FilterProps) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
+  return (
+    <Pressable style={styles.filterContainer} {...props}>
+      <Text
+        style={[isSelected ? styles.filterSelectedText : styles.filterText]}>
+        {children}
+      </Text>
+      <MaterialIcons
+        name="keyboard-arrow-down"
+        size={22}
+        color={isSelected ? colors[theme].BLUE_500 : colors[theme].GRAY_300}
+      />
+    </Pressable>
+  );
+}
+
+export const CompoundOption = Object.assign(OptionModal, {
+  Container,
+  Background,
+  Button,
+  Title,
+  Divider,
+  CheckBox,
+  Filter,
+});
+
+const styling = (theme: ThemeMode) =>
+  StyleSheet.create({
+    optionBackground: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      backgroundColor: 'rgba(0 0 0 / 0.5)',
+    },
+    optionContainer: {
+      borderRadius: 15,
+      marginHorizontal: 10,
+      marginBottom: 10,
+      backgroundColor: colors[theme].GRAY_100,
+      overflow: 'hidden',
+    },
+    optionButton: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 50,
+      gap: 5,
+    },
+    optionButtonPressed: {
+      backgroundColor: colors[theme].GRAY_200,
+    },
+    optionText: {
+      fontSize: 17,
+      color: colors[theme].BLUE_500,
+      fontWeight: '500',
+    },
+    dangerText: {
+      color: colors[theme].RED_500,
+    },
+    titleContainer: {
+      alignItems: 'center',
+      padding: 15,
+    },
+    titleText: {
+      fontSize: 16,
+      fontWeight: '500',
+      color: colors[theme].BLACK,
+    },
+    border: {
+      borderBottomColor: colors[theme].GRAY_200,
+      borderBottomWidth: 1,
+    },
+    checkBoxContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingVertical: 10,
+      paddingHorizontal: 30,
+      gap: 10,
+    },
+    checkBoxText: {
+      color: colors[theme].BLACK,
+      fontSize: 15,
+    },
+    filterContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+      gap: 5,
+    },
+    filterText: {
+      color: colors[theme].GRAY_300,
+      fontSize: 15,
+      fontWeight: '500',
+    },
+    filterSelectedText: {
+      color: colors[theme].BLUE_500,
+      fontSize: 15,
+      fontWeight: '500',
+    },
+  });
